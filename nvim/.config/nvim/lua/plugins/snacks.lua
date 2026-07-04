ac7564d06b941f5cf15b6514c521ca042ac14481
Exiles.prx <exiles.prx@gmail.com>
2026-07-04 12:13:54 -0400

    feat: snacks config

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+            ignored = false,
+          },
+        },
+        enabled = true,
+        input = {
+          enabled = true,
+        },
+      },
+    },
+  },
+}
